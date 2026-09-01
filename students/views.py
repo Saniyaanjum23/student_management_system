1e10502bef735233d78e0d91d29b02bcfb9d1ad7
Saniyaanjum23 <shaiksaniya2305@gmail.com>
2026-09-01 15:19:18 +0530

    first comit

---
+++ students/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render 
+
+def home(request):
+    course_names= [
+        'Database ManagementSystems',
+        'Operating Systems',
+        'Web Technologies',
+        'Computer Networks',
+        'Software Engineering',
+    ]
+    return render(request, 'students/home.html', {'course_names': course_names})
+
+def about(request):
+    return render(request, 'students/about.html')
+
+def contact(request):
+    return render(request, 'students/contact.html')
